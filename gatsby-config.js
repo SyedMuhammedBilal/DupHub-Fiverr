55f946adaa36c558fb5ce1667d12d54fdcb794e0
Syed Muhammed Bilal <intergo878@gmail.com>
2020-11-09 12:58:40 +0500

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "SWAPI",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "swapi",
+        // Url to query from
+        url: "https://api-eu-central-1.graphcms.com/v2/ckgdrm9afhn1101z853ug34gg/master",
+      },
+    },
+  ],
+}
